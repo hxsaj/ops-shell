5aa02c1f80bddf2d790fb7ee52309caebf0bb5ca
HG-bd <hxsaj@126.xom>
2020-08-29 02:29:57 +0800

    docker-ce安装脚本

--- docker-ce_install_yum.sh
+++ docker-ce_install_yum.sh
@@ -95,3 +95,32 @@ Docker_Install_without_Network(){
 # ypm -ivh container-selinux-2.68-1.el7.noarch.rpm
 # ypm -ivh libtool-ltdl-2.4.2-22.el7_3.x86_64.rpm
 # ypm -ivh docker-ce-18.06.1.ce-3.el7.x86_64.rpm
+
+
+# ---------------------------------------------------------------#
+
+echo "
+1，下载 docker-de 安装软件
+2，在线安装docker-ce
+3，离线安装docker-ce
+
+"
+# 选择服务
+read -t 30 -p  "选择需要的服务，按序号即可（都不需要请输入任意非数字字符 ）：" Choices_Service
+
+
+# 选择结果
+case ${Choices_Service} in
+
+    # 下载 docker-de 安装软件
+    "1" ) Docker_Download_Network             ;;
+
+    # 在线安装docker-ce
+	"2" ) Docker_Install_with_Network         ;;
+
+    # 离线安装docker-ce
+	"3" ) Docker_Install_without_Network      ;;
+
+    # 提示并退出
+	* ) echo "退出脚本，欢迎再次使用！" &&exit   ;;
+esac
